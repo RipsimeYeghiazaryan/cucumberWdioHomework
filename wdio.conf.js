5c980071343e20efda126f7bf75259426d0709fa
Hripsime Yeghiazaryan <hripsime_yeghiazaryan@epam.com>
2021-11-22 10:48:17 +0400

    add jenkins screenshot

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,5 +1,6 @@
 const allure = require('allure-commandline');
 exports.config = {
+
     //
     // ====================
     // Runner Configuration
@@ -112,7 +113,7 @@ exports.config = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     services: ['chromedriver'],
-    // services: ['selenium-standalone'],
+    // services: ['browserStack'],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
